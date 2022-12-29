0e68f2831e2d77e1a74f7b38227afaa53173fa64
Arman Fidanoglu <armanfidanoglu@gmail.com>
2022-12-29 19:51:26 +0000

    Created swipe navigation system

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback, useRef, useState} from 'react';
 import './App.css';
 
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Navigation, HashNavigation} from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, _] = useState("#");
+    console.log(state);
+    return (
+        <div className="App">
+            <a href="#" id="prev-vertical" className={state === "#" ? "" : "nav-button-disabled"}>Prev</a>
+            <a href="#" id="next-vertical">Next</a>
+            <Swiper
+                className="mySwiper swiper-h"
+                spaceBetween={0}
+                initialSlide={1}
+                modules={[Navigation, HashNavigation]}
+                navigation={true}
+                hashNavigation={{
+                    watchState: true,
+                }}
+            >
+                <SwiperSlide data-hash="events">Horizontal Slide 1</SwiperSlide>
+                <SwiperSlide>
+                    <Swiper
+                        className="swiper-v"
+                        direction={"vertical"}
+                        spaceBetween={0}
+                        initialSlide={1}
+                        modules={[Navigation, HashNavigation]}
+                        navigation={{
+                            enabled: true,
+                            nextEl: '#next-vertical',
+                            prevEl: '#prev-vertical',
+                            disabledClass: 'nav-button-disabled'
+                        }}
+                        hashNavigation={{
+                            watchState: true,
+                        }}
+                    >
+                        <SwiperSlide data-hash="music">Vertical Slide 3</SwiperSlide>
+                        <SwiperSlide data-hash="">Vertical Slide 2</SwiperSlide>
+                        <SwiperSlide data-hash="contact">Vertical Slide 1</SwiperSlide>
+
+                    </Swiper>
+                </SwiperSlide>
+                <SwiperSlide data-hash="software">Horizontal Slide 3</SwiperSlide>
+            </Swiper>
+        </div>
+    );
 }
 
 export default App;
