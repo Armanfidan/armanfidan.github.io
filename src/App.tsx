50ec39ff381ec614330564fced069e2f37db3a1f
Arman Fidanoglu <armanfidanoglu@gmail.com>
2022-12-30 10:33:54 +0000

    Finalised basic navigation system mechanics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useRef, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import {Swiper, SwiperSlide} from "swiper/react";
@@ -8,21 +8,30 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 function App() {
-    const [state, _] = useState("#");
-    console.log(state);
+    const [slide, setSlide] = useState([1, 1]);
+
+    // const slideTo = (index: number) => swiper.slideTo(index);
     return (
         <div className="App">
-            <a href="#" id="prev-vertical" className={state === "#" ? "" : "nav-button-disabled"}>Prev</a>
-            <a href="#" id="next-vertical">Next</a>
+            <a href="#" id="prev-horizontal" className={slide[1] === 1 ? "" : "nav-button-disabled"}>Prev</a>
+            <a href="#" id="next-horizontal" className={slide[1] === 1 ? "" : "nav-button-disabled"}>Next</a>
+            <a href="#" id="prev-vertical" className={slide[0] === 1 ? "" : "nav-button-disabled"}>Prev</a>
+            <a href="#" id="next-vertical" className={slide[0] === 1 ? "" : "nav-button-disabled"}>Next</a>
             <Swiper
                 className="mySwiper swiper-h"
                 spaceBetween={0}
                 initialSlide={1}
                 modules={[Navigation, HashNavigation]}
-                navigation={true}
+                navigation={{
+                    enabled: true,
+                    nextEl: '#next-horizontal',
+                    prevEl: '#prev-horizontal',
+                    disabledClass: 'nav-button-disabled'
+                }}
                 hashNavigation={{
                     watchState: true,
                 }}
+                onSlideChange={(index) => setSlide([index.snapIndex, slide[1]])}
             >
                 <SwiperSlide data-hash="events">Horizontal Slide 1</SwiperSlide>
                 <SwiperSlide>
@@ -41,10 +50,11 @@ function App() {
                         hashNavigation={{
                             watchState: true,
                         }}
+                        onSlideChange={(index) => setSlide([slide[0], index.snapIndex])}
                     >
-                        <SwiperSlide data-hash="music">Vertical Slide 3</SwiperSlide>
+                        <SwiperSlide data-hash="music">Vertical Slide 1</SwiperSlide>
                         <SwiperSlide data-hash="">Vertical Slide 2</SwiperSlide>
-                        <SwiperSlide data-hash="contact">Vertical Slide 1</SwiperSlide>
+                        <SwiperSlide data-hash="contact">Vertical Slide 3</SwiperSlide>
 
                     </Swiper>
                 </SwiperSlide>
