381e225c052376ca745404f2ed11bb7b4439bfc8
Arman Fidanoglu <armanfidanoglu@gmail.com>
2023-09-26 15:39:01 +0100

    Updated CV, removed "Let's Talk" button

--- src/components/home/HomeButtons.tsx
+++ src/components/home/HomeButtons.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 export default function HomeButtons() {
     return (
         <div className="profile-card-buttons">
-            <a className="btn" href={require('../../assets/CV_Arman_Fidanoglu_June_2023.pdf')} target="_blank" rel="noreferrer">Download CV</a>
-            <a id="go-to-contact" className="btn" href="#contact">Let's Talk</a>
+            <a className="btn" href={require('../../assets/CV_Arman_Fidanoglu_Sep_2023.pdf')} target="_blank" rel="noreferrer">Download CV</a>
         </div>
     );
 }
