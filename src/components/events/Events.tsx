adb9e952b3c9a7768d0ba791c88b4e03b1456075
Arman Fidanoglu <armanfidanoglu@gmail.com>
2023-12-19 12:38:52 +0000

    Updated AYA Nights page

--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -10,7 +10,10 @@ function Events() {
                     <img style={{borderRadius: '0'}} src={require("../../assets/AYA_nights_logo_white.png")}
                          alt="AYA Nights Logo"/>
                 </div>
-                <a className="social" href="https://www.instagram.com/aya.nights"><BsInstagram/>Instagram</a>
+                <a className="social" href="https://www.instagram.com/aya.nights"><BsInstagram/>AYA Nights Instagram</a>
+                <img className="sidebar-picture" src={require("../../assets/aya_team.png")} alt="AYA Nights - Team"/>
+                <img className="sidebar-picture" src={require("../../assets/aya_cuckoo.png")}
+                     alt="AYA Nights - Cuckoo"/>
                 <img id="vibe" className="sidebar-picture" src={require("../../assets/aya_dj.jpeg")}
                      alt="AYA Nights DJ Deck"/>
                 <img className="sidebar-picture" src={require("../../assets/aya_vibe.jpeg")} alt="AYA Nights - Vibe"/>
@@ -19,19 +22,33 @@ function Events() {
             <div id="aya-content">
                 <h2>Event Management</h2>
                 <p>
-                    In my free time, I work on my event management initiative, <b style={{color: 'var(--colour-primary-light)'}}>AYA Nights</b>,
+                    In my free time, I work on my event management initiative, <b
+                    style={{color: 'var(--colour-primary-light)'}}>AYA Nights</b>,
                     where I organise nightclub events in various popular venues across London.
                     By partnering with high-end venues and designing a welcoming and lively space,
-                    we create a friendly atmosphere where a diverse crowd of students and young professionals can get together and
-                    party.
+                    we create a friendly atmosphere where a diverse crowd of students and young professionals can get
+                    together and party.
+                    <br/><br/>
+                    Our flagship line of events is AYA Turkish, which appeals to Turkish students and young
+                    professionals in the UK, looking for a Turkish party like never before.
+                    Across two years, my partners and I have grown our attendee base greatly and become the face of
+                    Turkish events in the UK.
+                    When any Turkish person is asked about "Turkish events in the UK", the first name that comes into
+                    their mind is AYA.
+                    <br/><br/>
+                    We have also ventured out into a mediterranean crowd, all the while maintaining our strong Turkish
+                    presence and brand.
+                    We are now further venturing into events with House music alongside Turkish music, and we keep
+                    refining our business strategy.
                 </p>
                 <h3>Our Vision and Mission</h3>
                 <p>
                     We recognise that young professionals in London have no means of getting together with
                     like-minded individuals in a nightclub setting, as the events are either too expensive and
-                    discriminatory, or very "studenty".
+                    exclusive, or very "studenty".
                     Our mission is to break this barrier and provide a platform for this demographic to
-                    get together and enjoy a night out in a beautiful and lively setting, in a way that was not possible
+                    get together and enjoy a night out in a high-end, beautiful and lively setting, in a way that was
+                    not possible
                     before.
                 </p>
                 <br/>
@@ -39,56 +56,99 @@ function Events() {
                 <br/>
                 <h2>Upcoming Events</h2>
                 <div className="aya-event">
-                    <h3>AYA Mediterranean: The Debut</h3>
+                    <h3>The House of AYA<br/>Maddox Club</h3>
                     <hr/>
                     <p>
-                        Bring on the Mediterranean!
+                        We have been working on putting together bigger events with every attempt. We started with
+                        Imperial's
+                        Student Union and made our way through various Mayfair clubs, upping the stakes with every
+                        event.
+                        We now have an established customer base and strong relationships with the most prestigious
+                        nightclubs in the UK.
                         <br/><br/>
-                        After huge demand by our audience, we are incredibly excited to finally be unveiling our
-                        newest event series, AYA Mediterranean! This series consists of an international crowd and international music.
-                        Think all of your favourites - Reggaeton, Commercial, R&B, Hip-Hop.
+                        We are finally ready to up the stakes once more with our latest event, for which we are renting
+                        out the most
+                        prestigious and famous Mayfair Club: Maddox.
                         <br/><br/>
-                        We're proud to be starting this new chapter with our first Mediterranean event, "The Debut".
-                        You will enjoy unmistakably good music in our partnering venue, Libertine London, on 2nd February 2023.
-                    </p>
-                </div>
-                <div className="aya-event">
-                    <h3>AYA Originals: The Return</h3>
-                    <hr/>
-                    <p>
-                        The first two events were just the beginning. We're excited to be back with
-                        the much anticipated comeback for our original series with Turkish music.
+                        We will be taking over both rooms of Maddox, with our beloved DJ Jeremy Kohn playing Turkish
+                        music in the Main Room and
+                        our new and exciting lineup of DJs taking the Green Room to new heights with house music.
                         <br/><br/>
-                        Join us on 23rd February for the next event in our flagship series, AYA Originals,
-                        which is now better than ever - we will be joined by Sess Nişantaşı DJ Serkan Baykal,
-                        who will be bring his amazing music straight from Sess.
+                        I will keep this page updated as we progress through this journey - but for now, this 400-person
+                        event is our most ambitious to date.
                     </p>
                 </div>
                 <br/>
                 <hr style={{width: '60%'}}/>
                 <br/>
-                <h2>Past Events</h2>
+                <h2>Our History and Success Story</h2>
                 <div className="aya-event">
-                    <h3>AYA Originals: The Reunion</h3>
+                    <h3>AYA Turkish<br/>The Cuckoo Club</h3>
                     <hr/>
                     <p>
-                        We're upping the stakes - get ready for an entirely new level of event!
-                        For our second event, we're partnering with Libertine London, an amazing nightclub on
-                        Oxford Street.
-                        Prepare for table service, champagne shows and amazing music.
+                        As we kept working our way up London's most famous and prestigious clubs, we partnered
+                        with the Mayfair club Cuckoo.
+                        After two sold-out events with them, our attendees were very happy, as we provided them with
+                        two amazing nights
+                        at this prestigious venue which people cannot even get into on its normal nigths.
                         <br/><br/>
-                        We can't wait to see all our lovely people at this event, where we are kicking off a new era!
+                        As we took over this venue, we filled it with Turkish music and our lovely crowd.
                     </p>
-
                 </div>
                 <div className="aya-event">
-                    <h3>AYA Originals: The Debut</h3>
+                    <h3>AYA Mediterranean<br/>Aures London</h3>
                     <hr/>
                     <p>
-                        Prepare for Turkish music at the heart of London!
-                        Join us for an unforgettable night with
-                        amazing people and great music.
-                        We know you have been missing this in London, and we have decided it's time to have our nights back!
+                        After two successful mediterranean events at Libertine, we decided to venture out and find more unique venues.
+                        We came across Aures London, which boasted an immersive event space with a surround sound system and 360-degree
+                        displays on all walls, which was an amazing addition to our event.
+                        This was a great experience for us and the attendees.
+                        <br/><br/>
+                        However, after seeing that Mediterranean events diluted our brand image, we decided to stick to what we know best: Turkish events.
+                        This has been a great lesson in terms of building and maintaining a brand image.
+                    </p>
+                </div>
+                <div className="aya-event">
+                    <h3>AYA Mediterranean<br/>Libertine</h3>
+                    <hr/>
+                    <p>
+                        Bring on the Mediterranean!
+                        <br/><br/>
+                        After great demand, we decided to venture out into an entirely new demographic of Mediterranean attendees.
+                        This event covered an international crowd and international music: Reggaeton, Commercial, R&B, Hip-Hop.
+                        <br/><br/>
+                        This series was successful and our attendees enjoyed the relaxed environment we created in Libertine.
+                    </p>
+                </div>
+                <div className="aya-event">
+                    <h3>AYA Turkish<br/>Libertine</h3>
+                    <hr/>
+                    <p>
+                        After getting our start in Imperial's Student Union as the Turkish Society of the university,
+                        We saw a great opportunity to continue organising events and growing our attendee base.
+                        This is why we took the leap of faith and decided to up the stakes.
+                        <br/><br/>
+                        For our second event, we partnered with Libertine London, a Mayfair club on Oxford Street.
+                        This club was our first with table service, champagne shows and amazing music.
+                        This was a great experience in managing multiple stakeholders and organisation.
+                        <br/><br/>
+                        We even decided to fly over DJ Serkan Baykal from the famous Istanbul Nightclub, Sess.
+                        This event was a sign of our ambition and kicked off a new era: AYA Nights!
+                    </p>
+                </div>
+                <div className="aya-event">
+                    <h3>Turkish Society Event<br/>Imperial College Union</h3>
+                    <hr/>
+                    <p>
+                        This event is what started it all.
+                        With two coursemates, Alp and Yusuf, I founded the Imperial College Turkish society,
+                        through which we then organised an extremely ambitious event in the Metric Nightclub of the Imperial College Union.
+                        This event contained Turkish music and 400 Turkish students which came from all over England and Scotland, only for this event.
+                        <br/><br/>
+                        As a very innovative and first-of-its-kind event, this event led to a wave of hundreds of copycats across the UK and Europe.
+                        I can happily say we started a movement!
+                        <br/><br/>
+                        This event would ultimately lead to what has become AYA Nights, our event management initiative.
                     </p>
                 </div>
             </div>
