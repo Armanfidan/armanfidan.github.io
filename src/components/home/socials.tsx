065d78c3e7ac0c291e1ffd7d1a6540167295128f
Arman Fidanoglu <armanfidanoglu@gmail.com>
2023-12-19 14:46:28 +0000

    Updated links and CV

--- src/components/home/socials.tsx
+++ src/components/home/socials.tsx
@@ -4,7 +4,7 @@ import {BsLinkedin, BsGithub} from 'react-icons/bs'
 function Socials() {
     return (
         <div className="socials">
-            <a className="social" href="https://www.linkedin.com/in/arman-fidanoglu-86117595/"><BsLinkedin/></a>
+            <a className="social" href="https://www.linkedin.com/in/armanfidan"><BsLinkedin/></a>
             <a className="social" href="https://github.com/Armanfidan"><BsGithub/></a>
         </div>
     );
